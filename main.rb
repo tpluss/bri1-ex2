9e26e943986d65550ea63e488bed355191f769b5
rom <rom@rom.(none)>
2015-06-21 15:30:00 +0500

    Убрано формирование хэша Убраны лишние импорты Сохранение картинки переведено на mechanize Изменена логика подсчёта статистики

--- main.rb
+++ main.rb
@@ -1,8 +1,6 @@
 #coding: utf-8
 
 require 'csv'
-require 'digest'
-require 'open-uri'
 require 'mechanize'
 
 # Парсер каталога продукции сайта http://piknikvdom.ru
@@ -11,7 +9,7 @@ require 'mechanize'
 #
 # Разделителем CSV-файла является знак \t.
 # Формат каталога:
-#   Раздел | Подраздел | Наименование | Адрес карточки | Адрес картинки | Хэш
+#   Раздел | Подраздел | Наименование | Адрес карточки | Адрес картинки
 class PiknikParser
 
   MAIN_URL = 'http://www.piknikvdom.ru'
@@ -36,11 +34,10 @@ class PiknikParser
 
     @catalog_file = CSV.open(@path, 'a+', {:col_sep => @sep})
 
-    # Чтобы не дёргать каждый раз файл, создадим массив хэшей, который будет
-    # индикатором наличия объекта в каталоге.
-    @saved = @catalog_file.readlines.collect{ |r| r[5] }
+    # Храним ссылки на карточки, чтобы отсекать дубли
+    @saved = @catalog_file.readlines.collect{ |r| r[2] }
 
-    #@goods_qnt = 50
+    @goods_qnt = 500
     return    
     @parsed = 0
 
@@ -48,11 +45,11 @@ class PiknikParser
     @mechanize.user_agent_alias = 'Windows Chrome'
     catalog_page = @mechanize.get(PRODUCTS_URL)
     catalog_page.search(SECTBLOCK_SEL).each do |sect_block|
-      sect_title = sect_block.at(SECTLINK_SEL).attributes['title']
-      #puts "Works on #{ sect_title }"
+      sect_title = sect_block.at(SECTLINK_SEL).attributes['title'].to_s
+      puts "Works on #{ sect_title }"
 
       sect_block.search(SUBSECT_SEL).each do |subsect_url|
-        #puts "  Parse #{ subsect_url.text }"
+        puts "  Parse #{ subsect_url.text }"
         href = subsect_url.attributes['href']
         # Хорошо бы переделать на итератор, типа yield в питоне, который бы брал
         # по товару, если не достиг лимита. Тогда бы это позволило забирать сюда
@@ -66,23 +63,17 @@ class PiknikParser
     subsect_page.search(GOODSCARD_SEL).each do |goods_link|
       href = goods_link.attributes['href'].to_s
       name = goods_link.at('img').attributes['alt'].to_s
-      # Зато без регэкспов :)
       # FIX: переход в карточку товара с последующим дёрганьем по css-селектору.
-      # Товары с no_photo можно пропускать?
       img_url = goods_link.attributes['style'].value.sub('background: url(', '')\
         .sub(') no-repeat center center', '').sub('/images/no_photo_2.png', '')
 
-      hash = Digest::SHA256.hexdigest(sect + subsect + name)
-      next if @saved.index(hash)
-      @catalog_file << [sect, subsect, name, href, img_url, hash].map{|e| e.gsub('\t', ' ')}
-      #puts "    #{ name }"
+      next if @saved.index(href)
+      @catalog_file << [sect, subsect, name, href, img_url]
+      puts "    #{ name }"
 
-      # FIX: Кириллица в url => 404.
-      if !img_url.empty?
-        img_data = open(URI.encode(MAIN_URL + img_url)).read
-        open("#{ @img_dir }/#{ hash }#{ File.extname(img_url) }", 'wb') do |file|
-          file << img_data
-        end
+      unless img_url.empty?
+        img_name = "#{ @img_dir }/#{ hash }#{ File.extname(img_url) }"
+        @mechanize.get(MAIN_URL + img_url).save(img_name)
       end
 
       @parsed +=1
@@ -92,42 +83,34 @@ class PiknikParser
 
     next_link = subsect_page.at(NEXTPAGE_SEL)
     if next_link
-      #puts "Go to page #{ next_link.attributes['href'] }"
-      parse_subsect(@mechanize.get(MAIN_URL + '/' + next_link.attributes['href']), sect, subsect)
+      next_href = next_link.attributes['href']
+      puts "Go to page #{ next_href }"
+      parse_subsect(@mechanize.get(MAIN_URL + '/' + next_href), sect, subsect)
     end
   end
 
   def get_row_by_hash(hash)
     return [] unless @saved.index(hash)
-    #rows.collect {|r| r if r[5] == hash }.compact[0]
-    CSV.open(@path, 'r', {:col_sep => @sep}).readlines.each do |r|
-      return r if r[5] == hash
-    end
+    CSV.open(@path, 'r', {:col_sep => @sep}).readlines.select{|r| r[5] == hash}.first
   end
 
   def stat
     puts "Catalog contains #{ @saved.size } goods."
     return if @saved.size.zero?
-    cur_sect, cur_subsect = nil, nil
-    sect_qnt, subsect_qnt = 0, 0
-    # Адский обход. Надо предварительно знать количество, а выводить потом.
-    # Нужен алгоритм формирования хэша.
-    CSV.open(@path, 'r', {:col_sep => @sep}).readlines.each do |r|
-      unless cur_subsect == r[1] #or last line
-        puts " Total: #{ subsect_qnt } (#{ '%.2f' %(100.to_f * subsect_qnt/sect_qnt) }%)." unless cur_subsect.nil?
-        cur_subsect = r[1]
-        puts "  #{ r[1] }"
-        subsect_qnt = 0
-        unless cur_sect == r[0] #or last line
-          puts "Total: #{ sect_qnt }" unless cur_sect.nil?
-          cur_sect = r[0]
-          puts r[0]
-          sect_qnt = 0
+
+    data = CSV.open(@path, 'r', {:col_sep => @sep}).readlines
+    catalog = Hash[data.collect{|r| [r[0], {:qnt => 0}]}]
+    data.each do |r|
+      catalog[r[0]][r[1]] = [] unless catalog[r[0]][r[1]]
+      catalog[r[0]][r[1]].push(r.drop(2))
+      catalog[r[0]][:qnt] += 1
+    end
+
+    catalog.each do |cat, cat_data|
+        puts "#{ cat }: #{ cat_data.delete(:qnt) }"
+        cat_data.each do |subcat, subcat_data|
+            puts "  #{ subcat }: #{ subcat_data.size }"
         end
-      end
-      sect_qnt +=1
-      subsect_qnt +=1
-      puts " -> #{ r[2] }"
     end
 
     img_list = Dir.glob(@img_dir + '/*.{jpg,jpeg,gif,png}') # с запасом
